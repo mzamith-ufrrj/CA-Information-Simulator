d6c2a47cf9d1675e770e3c92a4d5bb663baa43b3
mzamith-ufrrj <mzamith@ufrrj.br>
2025-04-01 11:43:35 -0300

    update 2025-04-01 update #1

--- C++/CA-SocialNetworkonLine-SEIR.v.3.4/exec/script.sh
+++ C++/CA-SocialNetworkonLine-SEIR.v.3.4/exec/script.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
+###############################################################################
+# by Marcelo Zamith - mzamith@ufrrj.br - April 1st, 2025
+# Script used to execute the inforamtion propagation on server
+# Command:
+# ./script.sh 1000 1 6
+###############################################################################
+
+
 #Variables defined by console parameters
 dir_home="/home/marcelo/Documents/02-Trabalhos/03.Develop/ACs/Information/sources/C++/CA-SocialNetworkonLine-SEIR.v.3.4/exec/" #the main server directory used to execute codes and instances
 exec_code="IModelCA++.exec" #Executable file name
@@ -8,10 +16,29 @@ json_file="config.template.json" #Do not erase. It is the config template
 new_json_file_name="config-$prefix.json"
 
 #parameters from console
-nodes=100
-power=1
-min_connections=6
+nodes=$1 #100
+power=$2 #1
+min_connections=$3 #6
+echo "<Begin script>" >> log.txt
+#echo "nodes: $nodes / power $power /$min_connections"
+if [[ -z "$nodes" ]]; then
+   echo "variable nodes is not defined" >> log.txt
+   exit
+fi
+if [[ -z "$power" ]]; then
+   echo "variable power is not defined" >> log.txt
+   exit
+fi
+if [[ -z "$min_connections" ]]; then
+   echo "variable min_connections is not defined" >> log.txt
+   exit
+fi
 
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
 # Begin script
 if [ ! -d "$dir_out" ]; then
   mkdir $dir_out
@@ -19,15 +46,15 @@ fi
 
 #call R
 file_name="BA-N-$nodes-P-$power-C-$min_connections.net"
-echo "File name is: $file_name"
-echo "Call R"
+echo "File name is: $file_name" >> log.txt
+echo "Call R" >> log.txt
 if [ ! -f "$file_name" ]; then
-    Rscript Modelo-BarabasiAlbert.R $nodes $power $min_connections
+    Rscript Modelo-BarabasiAlbert.R $nodes $power $min_connections >> log.txt
 fi
 if [ -f $file_name ]; then
-    echo 'File was created'
+    echo 'File was created' >> log.txt
 else
-    echo 'Error'
+    echo 'Error' >> log.txt
 fi
 
 #reset output file
@@ -35,9 +62,9 @@ echo '' > "$new_json_file_name"
 
 #saving json file with new R .net file and simulation label
 sed -e "s/filename.net/$file_name/g" -e "s/simulationname/$prefix/g" "$json_file" > "$new_json_file_name"
-echo "$new_json_file_name was created"
-echo "Compute the simulation"
-$dir_home$exec_code $dir_home$new_json_file_name
+echo "$new_json_file_name was created" >> log.txt
+echo "Compute the simulation" >> log.txt
+$dir_home$exec_code $dir_home$new_json_file_name >> log.txt
 
 #Moving csv files to directory so that we can check
 mv "$dir_home/statistic.$prefix.csv"  $dir_out
@@ -47,4 +74,9 @@ fi
 #mv "$dir_home*.ppm"  $dir_out
 cp -f "$file_name"  $dir_out
 
-echo "<END-SIMULATION>" >> result.txt
+echo "<End script>" >> log.txt
+mv log.txt $dir_out
+
+
+#This output must be shown in client app through php output API
+echo "OUTPUT: $dir_out"
